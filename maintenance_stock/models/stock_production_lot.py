f766bc013493e1b7e018b40d51eca9cd69b84f5a
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2019-11-28 15:58:28 -0300

    [FIX] Hide button if source location is not Rental In

--- maintenance_stock/models/stock_production_lot.py
+++ maintenance_stock/models/stock_production_lot.py
@@ -17,6 +17,22 @@ class StockProductionLot(models.Model):
         string='Rentals',
         compute='_compute_maintenances'
     )
+    available_maintenance = fields.Boolean(
+        compute='_compute_available_for_maintenance'
+    )
+
+    @api.multi
+    def _compute_available_for_maintenance(self):
+        wh_id = self.env['stock.warehouse'].search(
+            [('company_id', '=', self.env.user.company_id.id)],
+            limit=1
+        )
+        available_location_id = wh_id.rental_in_location_id
+        for record in self:
+            if record.quant_location_id == available_location_id:
+                record.available_maintenance = True
+            else:
+                record.available_maintenance = False
 
     @api.multi
     def action_view_maintenances(self):
